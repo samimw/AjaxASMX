8362425b13200b06396914db84bd7cb5f3dfc254
samim <samim4@hotmail.com>
2014-04-14 17:49:14 +0100

    asmx webservice ajax

---
+++ App_Code/ajaxpost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[System.Web.Script.Services.ScriptService]
+public class ajaxpost : System.Web.Services.WebService {
+
+    [WebMethod]
+    public string Callback()
+    {
+        return "Hello World";
+    }
+    
+}
